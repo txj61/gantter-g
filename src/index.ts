f5764d7ab61a11d4d4423f496044c322c8b3c2ab
acwang <wanganchuan@deltaphone.com.cn>
2021-12-06 14:58:24 +0800

    test

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+/*
+ * @Author: Anthan
+ * @Date: 2021-12-06 14:43:41
+ * @LastEditTime: 2021-12-06 14:58:09
+ * @LastEditors: Anthan
+ * @Description:
+ */
 export default function () {
-  return 'hello world';
+  return 'hello world1';
 }
